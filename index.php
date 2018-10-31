f70b977756d486439d62d29a25d6b3b26ba077b6
root <root@lab-master01.sysage.com.tw>
2018-10-31 21:16:30 +0800

    pass ex280

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
  <head>
-  <title>Andre will passed the EX280</title>
+  <title>Andre will passed the EX280..!</title>
  </head>
  <body>
- <?php echo '<p>Andre passed the ex280 over 280 and over..</p>'; ?> 
+ <?php echo '<p>Andre passed the ex280 over 280 and over and over..</p>'; ?> 
  </body>
 </html>
